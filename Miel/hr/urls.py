5eb2517a15b3f5fc6e4c804fc8011d289331fac9
WIse <145248257+Sayrrexe@users.noreply.github.com>
2024-12-11 19:32:25 +0500

    feat: Added supervisor candidates retrieval with filtering
    - Implemented candidates retrieval for supervisors.
    - Added filtering by:
      - Courses (completed only).
      - Freshness (by creation date).
      - Age (exact value and range).
    - Created user role classes (supervisors, moderators).
    - Secured API with custom permission classes.

--- Miel/hr/urls.py
+++ Miel/hr/urls.py
@@ -7,16 +7,18 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'todos', views.TodoViewSet, basename='todo') # CRUD для todo
-router.register(r'favorites', views.FavoriteViewSet, basename='favorite') # CRUD для добавление кандидатов в избранное
-router.register(r'supervisors', views.SupervisorViewSet)  # CRUD для рук-лей ( от лица админа )
-router.register(r'candidates', views.CandidateViewSet, basename='candidate') #CRUD для кандидатов
+router.register(r'supervisor/favorites', views.FavoriteViewSet, basename='favorite') # CRUD для добавление кандидатов в избранное
+router.register(r'admin/supervisors', views.SupervisorViewSet)  # CRUD для рук-лей ( от лица админа )
+router.register(r'admin/candidates', views.CandidateViewSet, basename='candidate') #CRUD для кандидатов ( От лица админа )
 
 urlpatterns = [
     # подключение CRUD
-    path('api/v1/', include(router.urls)),# для Api
+    path('api/v1/', include(router.urls)),# для CRUD Api
+    
     # Рук-ли
-    path('api/v1/supervisors/', views.GetSupervisorInfoView.as_view()), # выдаёт всю информацию по рук-лю
-    path("api/v1/invitations/", views.InvitationAPIView.as_view()), # приглашен боссом кандидатов
+    path('api/v1/supervisor/candidates/', views.CandidateInfoView.as_view()), # список всех кандидатов
+    path('api/v1/supervisor/info', views.GetSupervisorInfoView.as_view()), # выдаёт всю информацию по рук-лю
+    path("api/v1/supervisor/invitations/", views.InvitationAPIView.as_view()), # приглашен боссом кандидатов
     
     # Модераторы
     path('api/v1/todo-stats/', views.TodoStatsView.as_view(), name='todo-stats'),
