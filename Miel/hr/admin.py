478442c9f5044b34707747a7f11f83f927f0a8b6
WIse <145248257+Sayrrexe@users.noreply.github.com>
2024-12-14 01:37:09 -0800

    Revert "Api (#4)" (#17)
    This reverts commit f02bb6d769b0cd1f3f0517cb9b83925ebcabae04.

--- Miel/hr/admin.py
+++ Miel/hr/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from . import models
 
-admin.site.register(models.CustomUser)
-admin.site.register(models.Supervisor)
-admin.site.register(models.Moderator)
-admin.site.register(models.Candidate)
-admin.site.register(models.Office)
-admin.site.register(models.Favorite)
-admin.site.register(models.Todo)
-admin.site.register(models.Invitation)
-admin.site.register(models.Transaction)
+# Register your models here.
