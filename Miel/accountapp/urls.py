38b37fe127da73689e69c37f580c05e9845baaf0
WIse <145248257+Sayrrexe@users.noreply.github.com>
2024-12-17 17:49:30 +0500

    fix: accountapp urls corrected, versions removed

--- Miel/accountapp/urls.py
+++ Miel/accountapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/login/', views.LoginAPIView.as_view(), name='api_login'),
-    path('api/v1/logout/', views.LogoutAPIView.as_view(), name='api_logout'),
-    path('api/v1/protected/', views.ProtectedAPIView.as_view(), name='protected'),
+    path('api/login/', views.LoginAPIView.as_view(), name='api_login'),
+    path('api/logout/', views.LogoutAPIView.as_view(), name='api_logout'),
+    path('api/protected/', views.ProtectedAPIView.as_view(), name='protected'),
 ]
