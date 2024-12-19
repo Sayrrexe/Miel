3d9bce09832e014c90cb5ff2d81611f7f6252432
WIse <145248257+Sayrrexe@users.noreply.github.com>
2024-12-19 21:04:10 +0500

    fix: change redirect urls

--- Backend/Miel/settings.py
+++ Backend/Miel/settings.py
@@ -113,9 +113,9 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirects
-LOGIN_REDIRECT_URL = '/admin/'
-LOGOUT_REDIRECT_URL = '/admin/'
-LOGIN_URL = '/admin/'
+LOGIN_REDIRECT_URL = '/api/admin/'
+LOGOUT_REDIRECT_URL = '/api/admin/'
+LOGIN_URL = '/api/admin/'
 
 # Logging
 LOGGING = {
