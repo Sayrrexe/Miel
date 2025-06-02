1cf6ad3cb145bd3b23741a8789e3245240fa1144
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-06-02 17:01:47 +0300

    добавлен JSDoc для input.tsx

--- Frontend/components/ui/input.tsx
+++ Frontend/components/ui/input.tsx
@@ -2,6 +2,15 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+/**
+ * Поле ввода с поддержкой рефов и настраиваемыми свойствами.
+ *
+ * @param {Object} props - Свойства компонента.
+ * @param {string} [props.className] - Дополнительные CSS классы.
+ * @param {string} [props.type] - Тип поля ввода.
+ * @param {React.Ref<HTMLInputElement>} ref - Реф для доступа к элементу ввода.
+ * @returns {JSX.Element} Поле ввода.
+ */
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
