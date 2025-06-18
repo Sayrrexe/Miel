c00df62fb9ba1a7101a4418098f90144d442fc53
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-06-18 12:20:54 +0300

    feat: выбор руководителя (селект) > добавлена кнопка с перенаправдением на страницу создания руководителя > [баги июнь 2025 Администрирование (Офисы)]

--- Frontend/components/shared/dashboardAdmin/adminAddingOffice/addingOffice.tsx
+++ Frontend/components/shared/dashboardAdmin/adminAddingOffice/addingOffice.tsx
@@ -121,6 +121,10 @@ export const AddingOffice = () => {
     }
   };
 
+  const handleAddAdminClick = () => {
+    router.push("/addingAdmin");
+  };
+
   return (
     <div className={cn("mt-20 ml-10")}>
       <Link
@@ -199,13 +203,13 @@ export const AddingOffice = () => {
           >
             Руководитель
           </label>
-          <div>
+          <div className="flex items-center gap-1">
             <Select
               onValueChange={(value) => setValue("supervisor", value)}
               defaultValue=""
               disabled={isLoading}
             >
-              <SelectTrigger className="w-full md:w-[450px] rounded-xl">
+              <SelectTrigger className="w-full md:w-[405px] rounded-xl">
                 <SelectValue placeholder="Выберите руководителя" />
               </SelectTrigger>
               <SelectContent>
@@ -234,10 +238,34 @@ export const AddingOffice = () => {
                 )}
               </SelectContent>
             </Select>
-            {errors.supervisor && (
-              <p className="text-error-text text-sm mt-1">{errors.supervisor.message}</p>
-            )}
+            <Button
+              type="button"
+              variant="ghost"
+              size="icon"
+              onClick={handleAddAdminClick}
+              aria-label="Добавить нового администратора"
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 5.25C12.4142 5.25 12.75 5.58579 12.75 6V11.25H18C18.4142 11.25 18.75 11.5858 18.75 12C18.75 12.4142 18.4142 12.75 18 12.75H12.75V18C12.75 18.4142 12.4142 18.75 12 18.75C11.5858 18.75 11.25 18.4142 11.25 18V12.75H6C5.58579 12.75 5.25 12.4142 5.25 12C5.25 11.5858 5.58579 11.25 6 11.25H11.25V6C11.25 5.58579 11.5858 5.25 12 5.25Z"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="0"
+                />
+              </svg>
+            </Button>
           </div>
+          {errors.supervisor && (
+            <p className="text-error-text text-sm mt-1">{errors.supervisor.message}</p>
+          )}
         </div>
         <div className={`flex gap-5 items-center ${css.inputDiv}`}>
           <label
