35bb245521a2ad70bdd450cf5a3101d12d0fbc89
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-06-27 22:43:08 +0300

    fix: стилизация таблицы [Баги июнь 2025 > Витрина кандидатов]

--- Frontend/components/shared/dashboardAdmin/candidatesAdmin/adminCandidatesMain.tsx
+++ Frontend/components/shared/dashboardAdmin/candidatesAdmin/adminCandidatesMain.tsx
@@ -48,16 +48,19 @@ export const AdminCandidates = () => {
   }, [setCandidates, token]);
 
   return (
-    <div className={`w-full max-w-[1000px] min-w-[950px] overflow-visible p-1 min-h-0 h-auto`}>
+    <div className={`w-full max-w-[1000px] min-w-[950px] overflow-visible p-1 pl-10 min-h-0 h-auto`}>
+      {/* инпут и кнопки */}
       <div className="pt-8 flex flex-row gap-4">
         <Input
-          className={`w-full basis-4/5 ml-10`}
+          type="search"
+          className={`w-full basis-4/5 `}
           placeholder="Найти кандидата"
           onChange={(e) => setSearch(e.currentTarget.value)}
         />
         <Button
+          type="submit"
           variant="secondary"
-          className={`w-full basis-2/5`}
+          className={`w-full basis-1/5`}
           onClick={() => {
             console.log(token);
             (async () => {
@@ -82,9 +85,10 @@ export const AdminCandidates = () => {
             })();
           }}
         >
-          {window.innerWidth < 1000 ? <Magnifier/> : "Поиск"}
+          {window.innerWidth < 1000 ? <Magnifier /> : "Поиск"}
         </Button>
         <Button
+          type="submit"
           variant="default"
           className={`w-full basis-2/5`}
           onClick={async () => {
@@ -94,31 +98,33 @@ export const AdminCandidates = () => {
           {window.innerWidth < 1000 ? "+" : "Добавить кандидата"}
         </Button>
       </div>
+      {/* таблица */}
       <div className={cn(css.tableContainer)}>
         <Table
-          className={`border-solid border-[#CACBCD] border-2 ml-10 w-[60vw] ${css.table}`}
+          className={`border-solid border-border-primary border w-full`}
         >
+          {/* Хэдер таблицы */}
           <TableHeader>
             <TableRow>
-              <TableHead className={`font-bold text-center ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 ФИО
               </TableHead>
-              <TableHead className={`font-bold ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Город
               </TableHead>
-              <TableHead className={`font-bold ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Телефон
               </TableHead>
-              <TableHead className={`font-bold ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Свободен
               </TableHead>
-              <TableHead className={`font-bold text-center ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Возраст
               </TableHead>
-              <TableHead className={`font-bold text-center ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Офис
               </TableHead>
-              <TableHead className={`font-bold ${css.tableHead}`}>
+              <TableHead className={`text-black text-base font-normal text-center tracking-tight`}>
                 Выбрать
               </TableHead>
             </TableRow>
@@ -131,7 +137,7 @@ export const AdminCandidates = () => {
               >
                 <Link href={`/addingEmployee/${objectData.id}`}>
                   <TableCell
-                    className={`flex items-center gap-3 ${css.tableCell}`}
+                    className={`text-black flex justify-start items-center gap-3`}
                   >
                     <span className="w-6">{""}</span>
                     {objectData.photo && objectData.photo != "-" ? (
@@ -139,12 +145,12 @@ export const AdminCandidates = () => {
                         src={objectData.photo}
                         width={39}
                         height={39}
-                        className={`rounded-3xl max-w-[90px] max-h-[90px] min-h-[39px] min-w-[39px] ${css.candidatePhoto}`}
+                        className={`flex rounded-3xl max-w-[90px] max-h-[90px] min-h-[39px] min-w-[39px] ${css.candidatePhoto}`}
                         alt="avatar"
                       />
                     ) : (
                       <Image
-                        className={`max-w-[90px] max-h-[90px] min-h-[39px] min-w-[39px] ${css.candidatePhoto}`}
+                        className={`flex max-w-[90px] max-h-[90px] min-h-[39px] min-w-[39px] ${css.candidatePhoto}`}
                         src={user}
                         width={39}
                         height={39}
@@ -155,26 +161,26 @@ export const AdminCandidates = () => {
                     <p className={css.candidateName}>{objectData.name}</p>
                   </TableCell>
                 </Link>
-                <TableCell className={css.tableCell}>
+                <TableCell className={`text-black text-center tracking-tight`}>
                   {objectData.city}
                 </TableCell>
-                <TableCell className={css.tableCell}>
+                <TableCell className={`text-black text-center tracking-tight`}>
                   <span className="w-6">{""}</span>
                   {objectData.phone}
                 </TableCell>
-                <TableCell className={css.tableCellCheck}>
+                <TableCell className={`text-black text-center tracking-tight`}>
                   <Checkbox
                     className="ml-5"
                     checked={false}
                   />
                 </TableCell>
-                <TableCell className={`text-center ${css.tableCellAge}`}>
+                <TableCell className={`text-black text-center tracking-tight`}>
                   {objectData.age} лет
                 </TableCell>
-                <TableCell className={`text-center ${css.tableCellCheck}`}>
-                  {objectData.office ? objectData.office : "нету"}
+                <TableCell className={`text-black text-center tracking-tight`}>
+                  {objectData.office ? objectData.office : "нет"}
                 </TableCell>
-                <TableCell className={css.tableCellCheck}>
+                <TableCell className={`text-black text-center tracking-tight`}>
                   <Checkbox className="ml-5" />
                 </TableCell>
               </TableRow>
