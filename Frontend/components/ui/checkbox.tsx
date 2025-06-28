db02a15fcaa2e2684959336bf9ccb47acfec74bb
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-06-28 11:34:39 +0300

    fix: стилизация чек-боксов [Баги июнь 2025 > Витрина кандидатов]

--- Frontend/components/ui/checkbox.tsx
+++ Frontend/components/ui/checkbox.tsx
@@ -13,15 +13,16 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "flex items-center justify-center h-4 w-4 rounded-none border border-btn-primary data-[state=checked]:bg-white data-[state=checked]:text-black transition-colors mx-auto",
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-ring",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
+      className={cn("flex items-center justify-center w-full h-full")}
     >
-      <Check className="h-4 w-4" />
+      <Check className="h-3.5 w-3.5" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
