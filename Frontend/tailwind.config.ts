f636dc54d655e3a6271f6801d28abfe07b2fae43
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-07-07 18:16:00 +0300

    feat: добавлена настройка доп. медиа-запроса (laptop)

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -85,6 +85,15 @@ const config: Config = {
         'normal': 'normal',
         'oblique': 'oblique',
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        'laptop': '1440px',
+        '2xl': '1536px',
+      },
+
     }
   },
   plugins: [require("tailwindcss-animate")],
