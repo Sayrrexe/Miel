bc55e6fe0e3c924d4f32e03916d73194706d9772
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-10-10 12:00:00 +0300

    fix: исправление ошибки пропажи хэдера > (plans) page.tsx

--- Frontend/app/(dashboard_adminka)/plans/page.tsx
+++ Frontend/app/(dashboard_adminka)/plans/page.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 
 const Dashboard_adminka = () => {
   return (
-    <div className={cn("overflow-y-auto h-screen pb-10    ")}>
+    <div className={cn("overflow-y-auto min-h-screen pb-10    ")}>
       <NewPlans />
     </div>
   );
