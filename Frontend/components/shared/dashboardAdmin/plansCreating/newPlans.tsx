d11f9e472171b787f1775875a2b713f2a99b1e50
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-10-15 17:15:46 +0300

    feat: рефакторинг (подключены TaskStatistics.tsx, TaskInsights.tsx) > newPlans.tsx

--- Frontend/components/shared/dashboardAdmin/plansCreating/newPlans.tsx
+++ Frontend/components/shared/dashboardAdmin/plansCreating/newPlans.tsx
@@ -5,9 +5,7 @@ import DatePicker from "react-date-picker";
 import "../../dataPicker/DatePicker.css";
 import "../../dataPicker/Calendar.css";
 import {Button} from "@/components/ui";
-import {Ellipsis} from "lucide-react";
 import { useTaskManagement } from '@/hooks/useTaskManagement';
-import css from "./main.module.css";
 import {Task, Value} from "@/types/api";
 import {TaskList} from "./TaskList";
 import {
@@ -16,6 +14,12 @@ import {
 import {
   EditTaskModal
 } from "@/components/shared/dashboardAdmin/plansCreating/EditTaskModal";
+import {
+  TaskStatistics
+} from "@/components/shared/dashboardAdmin/plansCreating/TaskStatistics";
+import {
+  TaskInsights
+} from "@/components/shared/dashboardAdmin/plansCreating/TaskInsights";
 
 export const NewPlans = () => {
   // Используем хук useTaskManagement
@@ -145,54 +149,8 @@ export const NewPlans = () => {
           />
         </div>
         <div className="flex flex-col gap-[148px]">
-          <div
-            className={`border-solid border-[1px] border-[#CACBCD] h-[208px] p-5 ${css.mainCircleDiv}`}
-          >
-            <div className="w-full flex justify-between text-center">
-              <p className="text-[#960047]">Успехи за неделю</p>
-              <Ellipsis className="text-[#798087]" />
-            </div>
-            <div className={`flex mt-5 gap-[6px] ${css.circleDiv}`}>
-              <div className="text-center">
-                <p className="text-sm font-bold mb-[10px]">Создано</p>
-                <div className="border-[#960047] border-solid border-[1px] h-[100px] w-[100px] rounded-full items-center flex flex-col">
-                  <p className="text-[#960047] text-4xl mt-4">
-                    {newTasks?.total_created || 0}
-                  </p>
-                  <p className="text-xs font-bold">задач</p>
-                </div>
-              </div>
-              <div className="text-center">
-                <p className="text-sm font-bold mb-[10px]">Завершено</p>
-                <div className="border-[#960047] border-solid border-[1px] h-[100px] w-[100px] rounded-full items-center flex flex-col">
-                  <p className="text-[#960047] text-4xl mt-4">
-                    {newTasks?.total_completed || 0}
-                  </p>
-                  <p className="text-xs font-bold">задач</p>
-                </div>
-              </div>
-              <div className="text-center">
-                <p className="text-sm font-bold mb-[10px]">Удалено</p>
-                <div className="border-[#960047] border-solid border-[1px] h-[100px] w-[100px] rounded-full items-center flex flex-col">
-                  <p className="text-[#960047] text-4xl mt-4">
-                    {newTasks?.total_deleted || 0}
-                  </p>
-                  <p className="text-xs font-bold">задач</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="border-solid border-[1px] border-[#CACBCD] p-5">
-            <p className="text-xl text-[#01BEC2]">Наблюдение</p>
-            <p className="mt-5">
-              Больше всего задач вы{" "}
-              <span className="text-[#01BEC2]">создаете</span> в{" "}
-              {newTasks?.max_created_day || ""}
-            </p>
-            <p>
-              Больше всего задач вы завершаете во {newTasks?.max_completed_day || ""}
-            </p>
-          </div>
+          <TaskStatistics stats={newTasks}/>
+          <TaskInsights stats={newTasks}/>
         </div>
       </div>
     </div>
